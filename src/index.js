3781decd431820bc7b3e5b399b720e82be6c1de1
CryptoTyler <info.enescan@gmail.com>
2023-06-06 17:52:22 +0300

    Last Commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import fetch from 'node-fetch';
 import * as cheerio from 'cheerio';
 
 //const url = 'https://t.me/Aavesome';
-const url = 'https://t.me/pepecoineth';
+//const url = 'https://t.me/pepecoineth';
+const cmcUrl = 'https://coinmarketcap.com/?page=2';
 
 const headers = {
   'Content-Type': 'application/json',
@@ -61,3 +62,53 @@ async function cleanStringToInt(string) {
     const result = parseInt(cleanedStr, 10);
     return result;
 }
+
+const fetchCMC = await fetcher(baseUrl,options)
+const $ = cheerio.load(fetchCMC)
+
+const links = await getAllLink(cmcUrl)
+
+async function getAllLink(url){
+    let baseUrl = url
+    const baseHref = $('base').attr('href')
+
+    if (baseHref) {
+        const isHrefAbsolute = /^[a-z][a-z0-9+.-]*:/.test(baseHref)
+        if (isHrefAbsolute) {
+        baseUrl = baseHref
+        } else if (baseHref === '/') {
+        baseUrl = (new URL(baseHref, url)).href
+        }
+    }
+
+    const urlArray = $('a')
+    .map((_i, el) => $(el).attr('href'))
+    .get()
+    .filter((href) => !!href)
+    .map((href) => {
+      // Throw a meaningful error when only a relative URL would be extracted instead of waiting for the Request to fail later.
+      // http htpps
+      const isHrefAbsolute = /^[a-z][a-z0-9+.-]*:/.test(href) // Grabbed this in 'is-absolute-url' package.
+      if (!isHrefAbsolute && !baseUrl) {
+        throw new Error(`An extracted URL: ${href} is relative and options.baseUrl is not set. ` +
+                'Use options.baseUrl in enqueueLinks() to automatically resolve relative URLs.')
+      }
+      const tryAbsolute = () => {
+        try {
+          return (new URL(href, baseUrl)).href
+        } catch {
+          return undefined
+        }
+      }
+      return baseUrl
+        ? tryAbsolute()
+        : href
+    })
+    .filter((href) => !!href) || []
+    return urlArray
+}
+
+
+
+const result = getChannelStatus($page)
+debugger

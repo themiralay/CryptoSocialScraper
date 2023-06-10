4c2a0a6605efa291d1373fe9cd13b6122751ca80
CryptoTyler <info.enescan@gmail.com>
2023-06-11 01:38:45 +0300

    Cmc Page all social link get integrated
    db Object refactoring

--- src/index.js
+++ src/index.js
@@ -125,6 +125,87 @@ async function cmcCurrencyGetter(url){
     return cmcCryptoLinks
 }
 
+const telegramDomains= [
+    "telegram.me",
+    "telegram.org",
+    "telesco.pe",
+    "tg.dev",
+    "t.me"
+]
+
+const discordDomains = [
+    "discordapp.com",
+    "discordapp.net",
+    "discord.com",
+    "discord.gg",
+    "discord.media",
+    "discordstatus.com"
+]
+
+const redditDomains = [
+    "redd.it",
+    "redditblog.com",
+    "reddit.com",
+    "redditinc.com",
+    "redditmedia.com",
+    "redditstatic.com",
+    "redditstatus.com"
+]
+
+const twitterDomains = [
+    "twttr.com",
+    "twitterinc.com",
+    "twitter.com",
+    "twitter.co",
+    "t.co",
+    "tweetdeck.com"
+]
+
+
+async function cmcDiscordTelegramGetter(url){
+    const urlSet = new Set();
+    const Crypto ={
+        name:"",
+        symbol:"",
+        telegram:{
+            pages:[],
+            onlineAverage:0,
+            memBerAverage:0
+        },
+        discord:{
+            pages:[],
+            onlineAverage:0,
+            memBerAverage:0
+        },
+        reddit:{
+            pages:[]
+        },
+        twitter:{
+            pages:[]
+        },
+    }
+    const fetchCMC = await fetcher(url,options)
+    const $ = cheerio.load(fetchCMC)
+    const filteredLinks = $('a[rel="nofollow noopener"]');
+    filteredLinks.each((index, element) => {
+        const href = $(element).attr('href');
+        urlSet.add(href)
+    });
+    for (const item of urlSet) {
+        if(telegramDomains.includes(item)){
+            Crypto.telegram.push(item)
+        }else if(twitterDomains.includes(item)){
+            Crypto.telegram.push(item)
+        }else if(redditDomains.includes(item)){
+            Crypto.reddit.push(item)
+        }else if(redditDomains.includes(item)){
+            Crypto.reddit.push(item)
+        }
+    }
+    debugger
+
+}
+
 async function saveArrayToCSV(array, filePath) {
     const csvData = array.join('\n');
   
@@ -144,10 +225,11 @@ async function saveArrayToCSV(array, filePath) {
   }
 
 
+const cmcTelegram= await cmcDiscordTelegramGetter("https://coinmarketcap.com/currencies/cardano/")
+debugger
 for (let index = 1; index <= 60; index++) {
     const url = `${cmcUrl}?page=${index}`
     const cmcPageCoin = await cmcCurrencyGetter(url)
     await saveArrayToCSV(cmcPageCoin, 'output.csv');
 }
-debugger
 //const result = getChannelStatus($page)
